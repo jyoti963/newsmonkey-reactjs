ca5283493afdc9154bec92a800a615f4d34f05ab
jyoti963 <ambarishjyoti963@gmail.com>
2022-11-11 13:11:19 +0530

    newsmonkey app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import NavBar from './components/NavBar';
+import News from './components/News';
+// import { connect } from 'react-redux'
+
+export default class App extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <News/>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
